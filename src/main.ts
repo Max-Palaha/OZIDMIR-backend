dab1586f5c50f3b31d2cd37506eade673c6b142c
Galytskii Danylo <galytskyyd@gmail.com>
2021-09-14 21:12:37 +0300

    changed

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .addTag('country')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
