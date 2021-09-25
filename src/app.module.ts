c9074c75375d7e9f30d2a74d86f7a318c4f4cbae
NatName <natname69@gmail.com>
2021-09-25 15:12:51 +0300

    mongooose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { CrawlerModule } from './modules/crawler/crawler.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpErrorFilter } from './helpers/http-error.filter';
 import { LoggerInterceptor } from './helpers/logger.interceptor';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
     UsersModule,
     CrawlerModule,
   ],
