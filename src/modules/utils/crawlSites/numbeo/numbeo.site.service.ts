45f372b83877b0c05fa59227ca3ae88732f0760f
Max Palazchenko <maksim.pal0228@gmail.com>
2021-11-16 11:45:45 +0200

    numbeo crawler v0.1

--- src/modules/utils/crawlSites/numbeo/numbeo.site.service.ts
+++ src/modules/utils/crawlSites/numbeo/numbeo.site.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Page } from 'puppeteer';
 import { CrawlerServiceUtils } from '../../crawler/crawler.utils.service';
 
 @Injectable()
 export class SiteNumbeoService {
-  private readonly url = 'https://www.numbeo.com/';
+  private readonly SITE_URL = 'https://www.numbeo.com/';
   constructor(private crawlerServiceUtil: CrawlerServiceUtils) {}
 
   async scrapeSite(url: string) {
-    await this.crawlerServiceUtil.crawl(url);
+    const page: Page = await this.crawlerServiceUtil.crawl(this.SITE_URL);
 
     return true;
   }
