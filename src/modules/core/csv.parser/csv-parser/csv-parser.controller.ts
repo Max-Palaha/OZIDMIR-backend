48321a47336a4a71aa7442b30b87e7189c34eef7
skkilett <skkilett3@gmail.com>
2021-11-24 21:25:15 +0200

    parser_v2

--- src/modules/core/csv.parser/csv-parser/csv-parser.controller.ts
+++ src/modules/core/csv.parser/csv-parser/csv-parser.controller.ts
@@ -1,24 +1,16 @@
-import { Controller, Post, Res, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CsvParserService } from './csv-parser.service';
 import { Express } from 'express';
-import * as csv from 'csv-parser';
+import { ApiOperation } from '@nestjs/swagger';
 
-const results = [];
 @Controller('csv-parser')
 export class CsvParserController {
   constructor(private csvParserService: CsvParserService) {}
+  @ApiOperation({ summary: 'parse csv' })
   @Post('parse')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File, @Res() res) {
-    console.log(file);
-    const fileStream = await this.csvParserService.bufferToStream(file.buffer);
-    console.log(fileStream);
-    fileStream
-      .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', () => {
-        console.log(results);
-      });
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.csvParserService.parse(file.buffer);
   }
 }
--- src/modules/core/csv.parser/csv-parser/csv-parser.service.ts
+++ src/modules/core/csv.parser/csv-parser/csv-parser.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { Readable } from 'stream';
+import * as csv from 'csv-parser';
 
 @Injectable()
 export class CsvParserService {
-  async bufferToStream(buffer) {
+  private async bufferToStream(buffer) {
     const stream = new Readable();
     stream.push(buffer);
     stream.push(null);
     return stream;
+  }
+
+  async parse(buffer) {
+    const stream = await this.bufferToStream(buffer);
+    const res = await this.promiceStream(stream);
+    return res;
+  }
+  async promiceStream(stream) {
+    const results = [];
+    return new Promise((resolve) => {
+      const streamPromice = stream.pipe(csv()).on('data', (data) => {
+        results.push(data);
+      });
+      streamPromice.on('end', () => {
+        resolve(results);
+      });
+    });
   }
 }
